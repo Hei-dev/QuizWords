d1a9d1ecdf60cfe305ebb3756466434df5e71136
Hei-dev <anpromer71@gmail.com>
2022-01-09 21:38:04 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,7 @@
 var qBanks = [
     //Question : Answer
-    {"Question":"Hello <b>World</b>!","Ans":["World"]},
-    {"Question":"<b>My</b> bad.","Ans":["My","my"]},
-    {"Question":"Let's <b>Go</b>","Ans":["Go"]},
+    {"Question":"多情應笑我，早生<b>華</b>髮","Ans":["花白","白"]},
+    {"Question":"故為之文以<b>志</b>","Ans":["記載"]},
+    {"Question":"月既不解飲，影<b>徒</b>隨我身","Ans":["白白","白白地","只"]},
+    {"Question":"<b>與</b>羸而不助五國也","Ans":["幫助","相助"]},
 ]
